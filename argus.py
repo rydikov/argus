b200acca7f40e22b5e0cf968c1eea379fd68a8cb
Maxim Rydikov <rydikov@gmail.com>
2020-11-05 17:16:45 +0300

    refactoring

--- argus.py
+++ argus.py
@@ -95,18 +95,18 @@ class BadFrameChecker(object):
         self.last_image_sizes = collections.deque([], self.store_images)
         self.deviation_percent = 60
 
-    def is_size_larger_avg(self, image_size):
+    def is_image_size_less_avg_size(self, image_size):
         avg_image_size = sum(self.last_image_sizes)/self.store_images
-        return (image_size/avg_image_size)*100 > self.deviation_percent
+        return (image_size/avg_image_size)*100 < self.deviation_percent
 
     def is_bad(self, image_path):
         image_size = os.path.getsize(image_path)
         self.last_image_sizes.appendleft(image_size)
 
-        if len(self.last_image_sizes) < self.store_images or self.is_size_larger_avg(image_size):
+        if len(self.last_image_sizes) < self.store_images:
             return False
         
-        return True
+        return self.is_image_size_less_avg_size(image_size):
 
 
 bfc = BadFrameChecker()
