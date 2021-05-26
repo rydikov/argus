502444ab2a0976451d13adaf945eb5b3e1ee8050
Maxim Rydikov <rydikov@gmail.com>
2021-05-26 12:01:38 +0300

    update checker

--- argus/helpers/bad_frame_checker.py
+++ argus/helpers/bad_frame_checker.py
@@ -1,3 +1,4 @@
+import cv2
 import logging
 import pytesseract
 
@@ -10,11 +11,11 @@ logger = logging.getLogger(__file__)
 def check_bad_frame(frame):
     h, w = frame.shape[0], frame.shape[1]
 
-    # Year box on frame
+    # Day of week name box on frame
     y_min_percent = 4
     y_max_percent = 12.5
-    x_min_percent = 16
-    x_max_percent = 26
+    x_min_percent = 26
+    x_max_percent = 35
 
     y_min = round(h * y_min_percent / 100)
     y_max = round(h * y_max_percent / 100)
@@ -23,12 +24,15 @@ def check_bad_frame(frame):
 
     frame = frame[y_min:y_max, x_min:x_max]
 
-    # psm 8: Treat the image as a single word.
-    recognized_year = pytesseract.image_to_string(
-        frame, 
-        config='--psm 8 --oem 3 -c tessedit_char_whitelist=0123456789'
-    ).rstrip()
+    def _recognize(frame):
+        # psm 8: Treat the image as a single word.
+        return pytesseract.image_to_string(frame, config='--psm 8').rstrip()
     
-    logger.info('Recognized year: {}'.format(recognized_year))
+    recognized_day_of_week = _recognize(frame)
+    if not bool(recognized_day_of_week):
+	    frame = cv2.bitwise_not(frame) # Invert image
+	    recognized_day_of_week = _recognize(frame)
+    
+    logger.info('Recognized day of week: {}'.format(recognized_day_of_week))
 
-    return not bool(recognized_year)
+    return not bool(recognized_day_of_week)
