5f689c97f4cb8f80934149742452101934f59449
Maxim Rydikov <rydikov@gmail.com>
2021-08-06 12:28:35 +0300

    set format for loki

--- argus/run.py
+++ argus/run.py
@@ -8,8 +8,6 @@ from argus import app
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(message)s',
-    datefmt='%d/%m/%Y %H:%M:%S'
 )
 logger = logging.getLogger(__file__)
 
