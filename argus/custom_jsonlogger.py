581fb011c921f5f6ba43ff44655f99cc120cca8f
Maxim Rydikov <rydikov@gmail.com>
2021-08-09 18:45:41 +0300

    add new formatter

---
+++ argus/custom_jsonlogger.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from pythonjsonlogger.jsonlogger import JsonFormatter
+
+
+class RFC3339JsonFormatter(JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(RFC3339JsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
