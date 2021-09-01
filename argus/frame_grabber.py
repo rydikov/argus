9570e8f56969f676fa7625a01a139a80158f1f11
Maxim Rydikov <rydikov@gmail.com>
2021-09-01 16:54:40 +0300

    reconnect time

--- argus/frame_grabber.py
+++ argus/frame_grabber.py
@@ -2,9 +2,11 @@ import cv2
 import logging
 import sys
 
+from time import sleep
 from argus.helpers.timing import timing
 from argus.helpers.bad_frame_checker import BadFrameChecker
 
+RECONNECT_SLEEP_TIME = 1
 logger = logging.getLogger('json')
 
 
@@ -13,6 +15,9 @@ class FrameGrabber:
     def __init__(self, config):
 
         self.cap = cv2.VideoCapture(config['source'])
+
+        if not self.cap.isOpened():
+            raise Exception("Could not connect to camera: %s " % config['source'])
 
         if 'bfc' in config:
             self.bfc = BadFrameChecker(config['bfc'])
@@ -25,10 +30,12 @@ class FrameGrabber:
             __, frame = self.cap.read()
         except Exception:
             logger.exception("Unable to get frame. Exit from thread")
+            sleep(RECONNECT_SLEEP_TIME)
             sys.exit(1)
 
         if frame is None:
             logger.error('Empty frame. Exit from thread')
+            sleep(RECONNECT_SLEEP_TIME)
             sys.exit(1)
 
         if self.bfc is not None and self.bfc.check(frame):
