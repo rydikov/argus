c3103f9667dd10c0ff93a0c1719eac2c25821ae5
Maxim Rydikov <rydikov@gmail.com>
2025-11-20 18:18:29 +0300

    add comment

--- argus/services/telegram.py
+++ argus/services/telegram.py
@@ -1,30 +1,3 @@
-# import cv2
-# import requests
-
-
-# class TelegramService:
-#     def __init__(self, bot_token, bot_chat_id):
-#         self.bot_token = bot_token
-#         self.bot_chat_id = bot_chat_id
-
-#     def send_message(self, message):
-#         send_text = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&parse_mode=Markdown&text={}'.format(
-#             self.bot_token,
-#             self.bot_chat_id,
-#             message
-#         )
-#         response = requests.get(send_text)
-#         return response.json()
-
-#     def send_frame(self, frame, message):
-#         _, img = cv2.imencode('.JPEG', frame)
-#         data = {"chat_id": self.bot_chat_id, "caption": message}
-#         url = f'https://api.telegram.org/bot{self.bot_token}/sendPhoto'
-#         response = requests.post(url, data=data, files={'photo': img.tobytes()})
-#         return response.json()
-
-
-
 import cv2
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
@@ -33,6 +6,7 @@ from tempfile import NamedTemporaryFile
 
 
 class TelegramService:
+    # Async Service for nonblocking recognized thread, run with run_async in helpers from sync callback
     def __init__(self, bot_token, bot_chat_id):
         self.bot_token = bot_token
         self.bot_chat_id = bot_chat_id
